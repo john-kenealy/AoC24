2412607e4549ec9b5af4737c84d65024394c510a
john kenealy <kenealyjohn2@johndeere.com>
2025-11-21 22:34:28 -0600

    meh: this doesnt work but I want a point to recover it from maybe

--- ELFS/src/day/six.rs
+++ ELFS/src/day/six.rs
@@ -3,10 +3,10 @@ use std::{
     collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
-    isize, usize,
+    usize,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Route {
     seen_locations: HashSet<Position>,
     visit_count: usize,
@@ -38,23 +38,29 @@ pub fn solve() {
         visit_count: 0,
     };
 
-    let mut _blocks: HashSet<(usize, usize)> = HashSet::new();
+    let mut blocks: HashSet<(usize, usize)> = HashSet::new();
 
     let mut map = Map { grid: import() };
 
     use std::time::Instant;
     let now = Instant::now();
 
-    //problem_one(&data, &mut patrol);
-
-    let count = problem_one(&mut map.grid, &mut route);
+    problem_one(&mut map.grid, &mut route, &mut blocks);
 
     let elapsed = now.elapsed();
     println!("Part 1 Elapsed: {:.2?}", elapsed);
     println!("Part 2 Elapsed: {:.2?}", elapsed);
 
-    println!("Count of locations visited: {:?}", route.visit_count);
-    println!("Count of locations to obstruct: {:?}", count);
+    let mut part_1_locations: HashSet<(usize, usize)> = HashSet::new();
+    for location in &route.seen_locations {
+        part_1_locations.insert((location.y, location.x));
+    }
+
+    println!(
+        "Count of distinct locations visited: {:?}",
+        part_1_locations.len()
+    );
+    println!("Count of locations to obstruct: {:?}", blocks.len());
 }
 
 fn import() -> Vec<Vec<char>> {
@@ -67,7 +73,7 @@ fn import() -> Vec<Vec<char>> {
         .collect()
 }
 
-fn problem_one(data: &Vec<Vec<char>>, route: &mut Route) {
+fn problem_one(data: &Vec<Vec<char>>, route: &mut Route, blocks: &mut HashSet<(usize, usize)>) {
     let mut start: (usize, usize) = (42, 42);
     data.iter()
         .enumerate()
@@ -77,6 +83,7 @@ fn problem_one(data: &Vec<Vec<char>>, route: &mut Route) {
         });
 
     println!("start {:?}", start);
+
     patrol(
         data,
         route,
@@ -85,22 +92,34 @@ fn problem_one(data: &Vec<Vec<char>>, route: &mut Route) {
             x: start.1,
             direction: Cardinal::N,
         },
+        blocks,
+        (0, 0),
+        true,
     );
 }
 
-fn patrol(data: &Vec<Vec<char>>, route: &mut Route, mut current_position: Position) {
+fn patrol(
+    data: &Vec<Vec<char>>,
+    route: &mut Route,
+    mut current_position: Position,
+    blocks: &mut HashSet<(usize, usize)>,
+    block: (usize, usize),
+    test: bool,
+) -> bool {
     // 1. add current_position position to route
     add_to_route(route, &current_position);
 
     // 2. determine if we are at the end of the route
     if check_for_edge(&current_position, data.len(), data[0].len()) {
         println!("found edge.");
-        return;
+
+        return false;
     }
 
     // 3. determing next step
     let mut next_position = get_next_position(&current_position);
     let mut end_patrol: bool = false;
+    let mut infinite_loop: bool = true;
 
     // 3.1 check next spot for
     while data[next_position.y][next_position.x] == '#' {
@@ -108,6 +127,8 @@ fn patrol(data: &Vec<Vec<char>>, route: &mut Route, mut current_position: Positi
 
         if !add_to_route(route, &current_position) {
             println!("encountered infinite route");
+            blocks.insert(block);
+            infinite_loop = true;
             end_patrol = true;
             break;
         }
@@ -122,10 +143,29 @@ fn patrol(data: &Vec<Vec<char>>, route: &mut Route, mut current_position: Positi
     }
 
     if !end_patrol {
-        patrol(data, route, next_position);
+        if test {
+            let mut test_data = data.clone();
+            let mut test_route = route.clone();
+            let block = (next_position.y, next_position.x);
+            test_data[block.0][block.1] = '#';
+
+            patrol(
+                &test_data,
+                &mut test_route,
+                current_position,
+                blocks,
+                block,
+                false,
+            );
+        }
+
+        patrol(data, route, next_position, blocks, (0, 0), test);
     } else {
-        println!("found edge or infinite loop.")
+        println!("found edge or infinite loop.");
+        println!("{:?}", blocks.len());
     }
+
+    infinite_loop
 }
 
 fn next_cardinal_direction(current_direction: Cardinal) -> Cardinal {
@@ -195,229 +235,5 @@ fn check_for_edge(position: &Position, height: usize, width: usize) -> bool {
             }
         }
     };
-
     return false;
 }
-
-//fn add_position(
-//    data: &mut Vec<Vec<char>>,
-//    position: (usize, usize),
-//    direction: &str,
-//    route: &mut Route,
-//) {
-//    route.seen_locations.insert(position)
-//}
-
-#[allow(dead_code)]
-fn next_direction_checker<'a>(
-    data: &Vec<Vec<char>>,
-    direction: &'a str,
-    index: (usize, usize),
-) -> Option<((usize, usize), &'a str)> {
-    let i_index: (isize, isize) = (
-        index.0.try_into().expect("where number"),
-        index.1.try_into().expect("where number"),
-    );
-    let next_direction;
-
-    let try_i_index = match direction {
-        "up" => {
-            next_direction = "right";
-            (i_index.0 - 1, i_index.1)
-        }
-        "right" => {
-            next_direction = "down";
-            (i_index.0, i_index.1 + 1)
-        }
-        "down" => {
-            next_direction = "left";
-            (i_index.0 + 1, i_index.1)
-        }
-        _ => {
-            next_direction = "up";
-            (i_index.0, i_index.1 - 1)
-        }
-    };
-
-    if try_i_index.0 < 0 || try_i_index.1 < 0 {
-        return None;
-    }
-
-    let next_index: (usize, usize) = (
-        try_i_index.0.try_into().expect("where usize"),
-        try_i_index.1.try_into().expect("where usize"),
-    );
-
-    if next_index.0 == data.len() || next_index.1 == data[0].len() {
-        return None;
-    }
-
-    if data[next_index.0][next_index.1] == '#' {
-        next_direction_checker(data, next_direction, index)
-    } else {
-        Some((next_index, direction))
-    }
-}
-
-// ##############################################################
-// ##############################################################
-// ##############################################################
-// ##################         PART 2        #####################
-// ##############################################################
-// ##############################################################
-
-//fn problem_two(map: &mut Map) -> usize {
-//    let mut start: (usize, usize) = (42, 42);
-//    map.grid.iter()
-//        .enumerate()
-//        .for_each(|(i, row)| match row.iter().position(|e| *e == '^') {
-//            Some(j) => start = (i, j),
-//            _ => (),
-//        });
-//
-//    println!("start {:?}", start);
-//
-//    let mut blocks: HashSet<(usize, usize)> = HashSet::new();
-//
-//    let mut count: usize = 0;
-//
-//    traverse_two(
-//        map,
-//        start,
-//        0,
-//        &mut blocks,
-//        &mut (start.0 + 1, start.1),
-//        &mut count
-//    );
-//
-//    //count;
-//    //for ele in blocks.iter() {
-//    //    println!("block: {:?}", ele);
-//    //}
-//    println!("removed?: {:?}", blocks.remove(&start));
-//    blocks.len()
-//}
-//
-//fn traverse_two(
-//    map: &mut Map,
-//    index: (usize, usize),
-//    direction: usize,
-//    blocks: &mut HashSet<(usize, usize)>,
-//    block: &mut (usize, usize),
-//    count: &mut usize
-//) {
-//    // Update list of locations seen and incrememnt count of coordinates hit
-//
-//    let next_index;
-//    let next_direction;
-//    let mut probe_patrol = PatrolTwo {
-//        seen_locations: HashSet::new(),
-//    };
-//
-//    probe_patrol.seen_locations.insert((index, direction));
-//
-//    match next_direction_checker_two(map, direction, index) {
-//        Some(i) => {
-//            next_index = i.0;
-//            next_direction = i.1;
-//
-//            //add temporary block
-//            //println!("{:?}", data[next_index.0][next_index.1]);
-//            map.grid[next_index.0][next_index.1] = '#';
-//            if traverse_two_probe(map, index, direction, &mut probe_patrol, count) {
-//                if blocks.insert(next_index) {
-//                    //println!("block found: {:?}, up to {:?}", next_index, blocks.len());
-//                }
-//            }
-//
-//            // remove temporary block
-//            map.grid[next_index.0][next_index.1] = '.';
-//        }
-//        None => {
-//            return;
-//        }
-//    }
-//
-//    *count = 0;
-//    //println!("{:?}", count);
-//    traverse_two(map, next_index, next_direction,  blocks, block, count);
-//}
-//
-//fn traverse_two_probe(
-//    map: &Map,
-//    index: (usize, usize),
-//    direction: usize,
-//    probe_patrol: &mut PatrolTwo,
-//    count: &mut usize
-//) -> bool {
-//    //println!("Probe testing: {:?} direction: {:?}", index, direction);
-//    match next_direction_checker_two(map, direction, index) {
-//        Some(i) => {
-//            if probe_patrol.seen_locations.insert((i.0, i.1)) {
-//                //traverse_two_probe(data, i.0, i.1, patrol, probe_patrol, count)
-//            } else {
-//                *count += 1;
-//                //println!("repeat: {:?}", (index, direction));
-//            }
-//
-//            if *count > 100 {
-//                true
-//            } else {
-//                traverse_two_probe(map, i.0, i.1, probe_patrol, count)
-//            }
-//        }
-//        None => false,
-//    }
-//}
-//
-//fn next_direction_checker_two<'a>(
-//    map: &Map,
-//    direction: usize,
-//    index: (usize, usize)
-//) -> Option<((usize, usize), usize)> {
-//    let i_index: (isize, isize) = (
-//        index.0.try_into().expect("where number"),
-//        index.1.try_into().expect("where number"),
-//    );
-//    let next_direction: usize;
-//
-//    let try_i_index = match direction {
-//        0 => {
-//            next_direction = 1;
-//            (i_index.0 - 1, i_index.1)
-//        }
-//        1 => {
-//            next_direction = 2;
-//            (i_index.0, i_index.1 + 1)
-//        }
-//        2 => {
-//            next_direction = 3;
-//            (i_index.0 + 1, i_index.1)
-//        }
-//        _ => {
-//            next_direction = 0;
-//            (i_index.0, i_index.1 - 1)
-//        }
-//    };
-//
-//    //if !probe {println!("{:?}: probe, index: {:?}", probe, try_i_index);}
-//    if try_i_index.0 < 0 || try_i_index.1 < 0 {
-//        return None;
-//    }
-//
-//    let next_index: (usize, usize) = (
-//        try_i_index.0.try_into().expect("where usize"),
-//        try_i_index.1.try_into().expect("where usize"),
-//    );
-//
-//    if next_index.0 == map.grid.len() || next_index.1 == map.grid[0].len() {
-//        return None;
-//    }
-//
-//    if map.grid[next_index.0][next_index.1] == '#' {
-//        next_direction_checker_two(map, next_direction, index)
-//    } else {
-//        //println!("Next_direction: next index {:?}, probe: {:?}", next_index, probe);
-//        Some((next_index, direction))
-//    }
-//}
