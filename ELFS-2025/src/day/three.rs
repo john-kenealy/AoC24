5e2af55a8aa0a0f46b9a57216fe410b1e3fa1b27
john kenealy <kenealyjohn2@johndeere.com>
2025-12-03 10:50:54 -0600

    feat: combine

--- ELFS-2025/src/day/three.rs
+++ ELFS-2025/src/day/three.rs
@@ -3,7 +3,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[allow(dead_code)]
-pub fn solve() {
+//FIRST SOLUTION THAT SOLVES EITHER PART BASED ON INPUT.
+pub fn solve_either(length: usize) {
     let data = import("inputs/three");
 
     use std::time::Instant;
@@ -12,68 +13,7 @@ pub fn solve() {
     let mut answer = 0;
 
     for bank in data {
-        let joltage = bank
-            .iter()
-            .enumerate()
-            .fold((0, 0), |digits, (i, &battery)| {
-                if i < bank.len() - 1 {
-                    if battery > digits.0 {
-                        return (battery, 0);
-                    } else if battery > digits.1 {
-                        return (digits.0, battery);
-                    }
-                } else {
-                    if battery > digits.1 {
-                        return (digits.0, battery);
-                    }
-                }
-                digits
-            });
-        answer += (joltage.0.to_string() + &joltage.1.to_string())
-            .parse::<usize>()
-            .unwrap()
-    }
-
-    println!("Answer: {:?}", answer);
-
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
-}
-
-#[allow(dead_code)]
-fn import(path: &str) -> Vec<Vec<u32>> {
-    let doc = File::open(path).expect("where file");
-    let reader = BufReader::new(doc);
-
-    reader
-        .lines()
-        .map(|l| {
-            l.expect("where string")
-                .chars()
-                .map(|n| n.to_digit(10).unwrap())
-                .collect()
-        })
-        .collect()
-}
-
-//fn get_digit(bank: &String, index: usize) -> u32 {
-//   bank.chars().nth(index).unwrap().to_digit(10).unwrap()
-//}
-//
-//
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-#[allow(dead_code)]
-pub fn solve_two() {
-    let data = import("inputs/three");
-
-    use std::time::Instant;
-    let now = Instant::now();
-
-    let mut answer = 0;
-
-    for bank in data {
-        let joltage: String = joltage_builder(bank);
+        let joltage: String = joltage_builder(bank, length);
         answer += joltage.parse::<usize>().unwrap();
     }
 
@@ -83,11 +23,11 @@ pub fn solve_two() {
     println!("Elapsed: {:.2?}", elapsed);
 }
 
-fn joltage_builder(bank: Vec<u32>) -> String {
+fn joltage_builder(bank: Vec<u32>, length: usize) -> String {
     let mut joltage = String::new();
     let mut start = 0;
 
-    for i in (0..12).rev() {
+    for i in (0..length).rev() {
         //println!("start {start}");
         let sub_bank = &bank[start..bank.len() - i];
         let digit = sub_bank.iter().max().unwrap();
@@ -104,4 +44,20 @@ fn joltage_builder(bank: Vec<u32>) -> String {
     }
     //println!("{joltage}");
     joltage
+}
+
+#[allow(dead_code)]
+fn import(path: &str) -> Vec<Vec<u32>> {
+    let doc = File::open(path).expect("where file");
+    let reader = BufReader::new(doc);
+
+    reader
+        .lines()
+        .map(|l| {
+            l.expect("where string")
+                .chars()
+                .map(|n| n.to_digit(10).unwrap())
+                .collect()
+        })
+        .collect()
 }
--- ELFS-2025/src/main.rs
+++ ELFS-2025/src/main.rs
@@ -3,5 +3,9 @@ mod day;
 fn main() {
     //day::one::solve_two_point_five();
     //day::two::solve_two();
-    day::three::solve_two();
+
+
+    //Input 2 for the part 1 solution, and 12 for part 2 solution, where the number is the length
+    //of string to build.
+    day::three::solve_either(12);
 }
